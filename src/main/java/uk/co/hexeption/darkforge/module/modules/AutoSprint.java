c70a183f0b588a6209d6a396bd8f00c3e15aa912
Xdarkwolf33 <xxsocercerxx@gmail.com>
2017-02-24 19:55:37 -0500

    Update AutoSprint.java

--- src/main/java/uk/co/hexeption/darkforge/module/modules/AutoSprint.java
+++ src/main/java/uk/co/hexeption/darkforge/module/modules/AutoSprint.java
@@ -1,1 +1,35 @@
+/*******************************************************************************
+ *     DarkForge a Forge Hacked Client
+ *     Copyright (C) 2017  Hexeption (Keir Davis)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 
+package uk.co.hexeption.darkforge.module.modules;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.multiplayer.PlayerControllerMP;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.*;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.*;
+import org.lwjgl.*;
+import uk.co.hexeption.darkforge.module.Module;
+import uk.co.hexeption.darkforge.*;
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class 
