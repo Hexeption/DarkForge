6b526035b319a9505978c9b879506de39836ef55
Hexeption <minecraftfun201@gmail.com>
2017-02-26 03:01:09 +0000

    :construction: The start of a gui

---
+++ src/main/java/uk/co/hexeption/darkforge/gui/gui/GuiManager.java
@@ -0,0 +1,13 @@
+package uk.co.hexeption.darkforge.gui.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+import uk.co.hexeption.darkforge.gui.gui.elements.Frame;
+
+import java.util.ArrayList;
+
+public class GuiManager extends GuiScreen {
+
+    private final ArrayList<Frame> frames = new ArrayList<>();
+
+
+}
---
+++ src/main/java/uk/co/hexeption/darkforge/gui/gui/elements/Text.java
@@ -0,0 +1,4 @@
+package uk.co.hexeption.darkforge.gui.gui.elements;
+
+public class Text {
+}
